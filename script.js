edaa31778f5147716777efe037e6079c6c5681d6
Sofia Andersson <soan022602@skovde.se>
2022-12-03 00:25:39 +0100

    CSS improvements and function for generating buttons from the array of letters.

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-
-
-
-
-
-
-    //Information
-    var guessInfo = "That was correct/wrong";
-    var newGameInfo = "New game started!"
-
-    var guess = '';
-    var wrong_guesses = 0;
-    var wrong_guesses_max = 6;
-    var guessed_letters = []
-
-    var guessing_words = [
+    var guessingWords = [
         "kitchen",
         "library",
         "couch",
@@ -38,28 +23,64 @@
         "wizard"
     ]
 
+    //Variables
+    var guessInfo = "That was correct/wrong";
+    var newGameInfo = "New game started!";
+    var guess = '';
+    var wrongGuesses = 0;
+    var wrongGuessesMax = 6;
+    var guessedLetters = []
+
+    //
+    var myTestKey = document.getElementById("testKey");
+    myTestKey.addEventListener("click", checkGuess);
+
+    //For starting the game
+    var myNewGameButton = document.getElementById("newGameButton");
+    myNewGameButton.addEventListener("click", newGame);
+
+    //Connecting key buttons HTML <--> JavaScript
+    var myForButtons = document.getElementById("forButtons")
+
+
+    //TEST
+    //const letterContainer = document.getElementById("letter-container");
+
     // Picks a random word from the guessing_words array.
     // The Math.random function result is multiplied by the length of the array.
     // The result of this multiplication is rounded down with the Math.floor function to get a valid index number to use. 
-    var random_index = Math.floor(Math.random() * guessing_words.length);
-    document.write(guessing_words[random_index] )
+    var random_index = Math.floor(Math.random() * guessingWords.length);
+    document.write(guessingWords[random_index])
 
     // Creates a array of letters from a string.
     // Split seperates each letter individually and join removes commas.
-    var buttons = 'abcdefghijklmnopqrstuvwxyz'.split('').join('')
+    var keys = 'abcdefghijklmnopqrstuvwxyz'.split('').join('')
 
     function newGame(){
         wrong_guesses = 0;
         wrong_guesses_max = 6;
-        guessed_letters = []
-        alert(newGameInfo);
+        guessed_letters = [];
+        alert(newGameInfo)
     }
 
-    function checkLetterGuess(){
+    function checkGuess(){
         alert(guessInfo);
     }
 
+   function createButtons(keys) {
+        for (var i = 0; i < keys.length; i++) {
+            var keyButtonElement = document.createElement("Button");
+            
+            //Enters the key character into the button
+            keyButtonElement.innerHTML = keys[i];
 
+            document.body.appendChild(keyButtonElement);
+        }
+   }
+   createButtons(keys);
+
+    //Variable testing
+    //var length = guessingWords.length
+    //document.write(length);
     //document.write(buttons[0])
     //document.write(buttons)
-
