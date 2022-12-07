83bf63da97b12629b2809a891246cb5c3593a20e
Sofia Andersson <soan022602@skovde.se>
2022-12-08 00:04:35 +0100

    Change word to guess to underline to hide it. Moved keyboard buttons into the game box.

--- script.js
+++ script.js
@@ -1,65 +1,47 @@
-    var guessingWords = [
-        "kitchen",
-        "library",
-        "couch",
-        "pineapple",
-        "balcony",
-        "rooftop",
-        "computer",
-        "germany",
-        "airplane",
-        "coconut",
-        "millionaire",
-        "spotlight",
-        "diamond",
-        "volcano",
-        "elephant",
-        "giraffe",
-        "crocodile",
-        "hummingbird",
-        "chimpanzee",
-        "christmas",
-        "halloween",
-        "wizard"
-    ]
+    //For starting the game
+    var myNewGameButton = document.getElementById("newGameButton");
+    myNewGameButton.addEventListener("click", newGame);
+    
+    //Connecting key buttons HTML <--> JavaScript
+    var myForButtons = document.getElementById("forButtons")
+
+    var wordBox = document.getElementById("wordBox")
+    var keyboardBox = document.getElementById("keyboardBox")
 
     //Variables
     var guessInfo = "That was correct/wrong";
     var newGameInfo = "New game started!";
-    var guess = '';
+    var helloWorld = "Helloooooo World!!";
+    var guessWord = '';
     var wrongGuesses = 0;
     var wrongGuessesMax = 6;
     var guessedLetters = []
+    
+    var guessingWords = [ "kitchen", "library", "couch", "pineapple", "balcony", "rooftop", "computer", "germany", "airplane", "coconut", "millionaire", "spotlight", "diamond", "volcano", "elephant", "giraffe", "crocodile", "hummingbird", "chimpanzee", "christmas", "halloween", "wizard" ]
 
-    //
-    var myTestKey = document.getElementById("testKey");
-    myTestKey.addEventListener("click", checkGuess);
-
-    //For starting the game
-    var myNewGameButton = document.getElementById("newGameButton");
-    myNewGameButton.addEventListener("click", newGame);
-
-    //Connecting key buttons HTML <--> JavaScript
-    var myForButtons = document.getElementById("forButtons")
-
-
-    //TEST
-    //const letterContainer = document.getElementById("letter-container");
-
-    // Picks a random word from the guessing_words array.
+    // Picks a random word from the guessingWords array.
     // The Math.random function result is multiplied by the length of the array.
     // The result of this multiplication is rounded down with the Math.floor function to get a valid index number to use. 
     var random_index = Math.floor(Math.random() * guessingWords.length);
-    document.write(guessingWords[random_index])
+    var guessWord = guessingWords[random_index]
+    //alert(chosenWord);
 
-    // Creates a array of letters from a string.
+
+    // Replaces all letters of the word to guess with an underline
+    var hiddenWord = guessWord.replace(/./g, "_");
+    //document.write(hiddenWord);
+
+    // Send underline-converted guessWord to the wordBox div in HTML 
+    wordBox.innerHTML = hiddenWord;
+
+    // Creates an array of letters from a string.
     // Split seperates each letter individually and join removes commas.
     var keys = 'abcdefghijklmnopqrstuvwxyz'.split('').join('')
 
     function newGame(){
-        wrong_guesses = 0;
-        wrong_guesses_max = 6;
-        guessed_letters = [];
+        wrongGuesses = 0;
+        wrongGuessesMax = 6;
+        guessedLetters = [];
         alert(newGameInfo)
     }
 
@@ -69,15 +51,22 @@
 
    function createButtons(keys) {
         for (var i = 0; i < keys.length; i++) {
-            var keyButtonElement = document.createElement("Button");
-            
+            var keyButtonElement = document.createElement("button");
+
             //Enters the key character into the button
             keyButtonElement.innerHTML = keys[i];
 
-            document.body.appendChild(keyButtonElement);
+            //Sends each key to the keyboardBox div in HTML
+            keyboardBox.appendChild(keyButtonElement);
+
+            // What will happen when the user presses a key button?
+            //button.addEventListener("click", () => {
+            //    alert(helloWorld);
+            //}            
         }
    }
    createButtons(keys);
+   
 
     //Variable testing
     //var length = guessingWords.length
